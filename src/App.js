bae8629547d67725c363925693fd0f73c2b26a4c
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-10-18 16:36:23 +0100

    fetchInteractions fetch request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [interactions, setInteractions] = useState([]);
+
+  const fetchInteractions = async () => {
+    try {
+      let response = await fetch("https://substantive.pythonanywhere.com/");
+      let data = await response.json();
+      setInteractions(data.interactions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchInteractions();
+  }, []);
+
+  console.log("interactions", interactions);
+
   return <h1>Hello World!</h1>;
 }
 
