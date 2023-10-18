93417d461314b439b86a1b83409723638c24f703
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-10-18 17:12:17 +0100

    Initial doughnut chart setup with data props

--- src/components/DoughnutChart.js
+++ src/components/DoughnutChart.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
-function DoughnutChart() {
-  return <div>DoughnutChart</div>;
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function DoughnutChart({ sectorNames, sectorCounts }) {
+  const data = {
+    labels: sectorNames,
+    datasets: [
+      {
+        data: sectorCounts,
+        backgroundColor: "#800000",
+      },
+    ],
+  };
+
+  const options = {
+    legend: {
+      position: "right",
+      onClick: null,
+    },
+  };
+
+  return <Doughnut data={data} options={options} />;
 }
 
 export default DoughnutChart;
