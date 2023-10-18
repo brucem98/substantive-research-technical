de6718308503a187ff11d4dba781cc7101de4218
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-10-18 17:21:18 +0100

    Calculate percentages and add datalabels to chart

--- src/components/DoughnutChart.js
+++ src/components/DoughnutChart.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
 function DoughnutChart({ sectorNames, sectorCounts }) {
   const data = {
@@ -16,6 +17,27 @@ function DoughnutChart({ sectorNames, sectorCounts }) {
   };
 
   const options = {
+    plugins: {
+      datalabels: {
+        textAlign: "center",
+        color: "#000000",
+        font: {
+          weight: "bold",
+          size: 16,
+        },
+        formatter: (value, context) => {
+          const dataPoints = context.chart.data.datasets[0].data;
+          const totalSum = dataPoints.reduce((a, b) => a + b, 0);
+          const percentageValue = ((value / totalSum) * 100).toFixed(2);
+
+          return `${percentageValue}%`;
+        },
+      },
+      legend: {
+        position: "right",
+        onClick: null,
+      },
+    },
     legend: {
       position: "right",
       onClick: null,
